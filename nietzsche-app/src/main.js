918dd94136d0ad2202e1e19aa22cd64421379a94
Sara Chodosh <sechodosh@gmail.com>
2021-03-24 14:09:30 -0400

    updated svelte app

--- nietzsche-app/src/main.js
+++ nietzsche-app/src/main.js
@@ -1,15 +1,19 @@
 import App from './App.svelte';
 import * as d3 from 'd3';
 
-d3.csv('./data.csv').then(d => {
-	console.log(d)
+fetch('https://raw.githubusercontent.com/sarachodosh/workshop1-nietzsche/main/data-scraping/data.json')
+.then((res) => res.json())
+.then((json) => {
+	const data = json;
+
+	const app = new App({
+		target: document.body,
+		props: {
+			data: data
+		}
+	});
 })
 
-const app = new App({
-	target: document.body,
-	props: {
-		data: data
-	}
-});
+
 
 export default app;
