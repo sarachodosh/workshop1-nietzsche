2f5cdff68577a2d2837a722a601f956675a8aeda
Sara Chodosh <sechodosh@gmail.com>
2021-03-27 10:05:03 -0400

    added indexes and rows for svelte grid

--- nietzsche-app/src/App.svelte
+++ nietzsche-app/src/App.svelte
@@ -4,17 +4,19 @@
     // import Rect-bar from './components/Rect-bar.svelte'
 
 	export let data;
-	console.log(data)
+	// console.log(data)
 	const testData = data[6]
 	// console.log(testData)
 	
-	// var i;
-	// function lineHandler(a) {
-	// 	for (i = 0; i < a.length; i++) {
-	// 		// return a[i]*122.926;
-	// 		console.log(a)
-	// 	}
-	// }
+	const pink = '#E12179';
+	const yellow = '#BB9F06';
+	const blue = '#087F8C';
+	const ltgrey = '#8F8F8F'
+	const dkgrey = '#414142'
+
+	heightScale = d3.scaleLinear()
+				.domain[d3.extent(data, d => d['length'])]
+				.range([height, 0])
 
 </script>
 <!-- <Rect-bar /> -->
@@ -22,12 +24,12 @@
 
 <main>
 	<svg viewBox='0 0 500 500'>
-		{#if testData}			
+		{#if data}			
 		{#each data as d}
-			<rect width='20' height={d.length/1000} fill='black' transform='translate({d.index}, 0)'/>
-			<!-- {#each data.schop_matches as e, i}
-				<rect width='20' height='1' transform='translate(0, {e.frac_schop[i]}'/>
-			{/each} -->
+			<rect width='15' height={d['length']/1000} fill={dkgrey} transform='translate({d.index*16}, 0)'/>
+			{#each d.schop_matches.matched_positions as e}
+				<rect width='15' height='1' transform='translate({d.index*16}, {e/1000})' fill={pink}/>
+			{/each}
 		{/each}
 		{/if}
 	</svg>
